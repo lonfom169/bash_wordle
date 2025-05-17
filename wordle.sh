2c5f710c312eb82ab1e960dbeb2aad39634579a2
lonfom169 <50217346+lonfom169@users.noreply.github.com>
2025-05-17 12:10:35 -0300

    cleanup

--- wordle.sh
+++ wordle.sh
@@ -3,40 +3,36 @@
 path=$(dirname $0)
 answer=$(shuf -n 1 $path/wordle_La.txt)
 alphabet='abcdefghijklmnopqrstuvwxyz'
-console="\033[38;2;66;255;255mGuess the word:\033[0m\n"
+console='\033[38;2;66;255;255mGuess the word:\033[0m\n'
 arg1='@@@@@'
 
 clear
-echo -e "$console"
+echo -e $console
 
 for i in {1..6}; do
     while true; do
         read -N 5 attempt
 
-        if [[ $attempt =~ [^a-zA-Z] ]]; then
-            echo -e "\033[38;2;240;0;0m Wrong input\033[0m"
-            read -n 1 -srp 'Press any key to continue'
-            clear
-            if [[ $alphabet_print != "" ]]; then
-                echo -e "$console$alphabet_print\n"
-            else
-                echo -e "$console"
-            fi
-            continue
-        fi
-
-        if (grep -Fxq "$attempt" "$path"/wordle.txt); then
+        if (grep -Fxq $attempt $path/wordle.txt); then
             console+="\n$attempt"
             break
         else
-            echo -e "\033[38;2;240;0;0m Not in word list\033[0m"
+            if [[ $attempt =~ [^a-zA-Z] ]]; then
+                echo -e '\033[38;2;240;0;0m Wrong input\033[0m'
+            else
+                echo -e '\033[38;2;240;0;0m Not in word list\033[0m'
+            fi
+
             read -n 1 -srp 'Press any key to continue'
             clear
-            if [[ $alphabet_print != "" ]]; then
+
+            if [[ $alphabet_print != '' ]]; then
                 echo -e "$console$alphabet_print\n"
             else
-                echo -e "$console"
+                echo -e $console
             fi
+
+            continue
         fi
     done
 
@@ -45,14 +41,15 @@ for i in {1..6}; do
 
     won=true
     gray=true
-    green_slots=""
-    slots=""
-    greens=""
-    yellows=""
+    green_slots=''
+    slots=''
+    greens=''
+    yellows=''
 
     for a in {0..4}; do
         char=${attempt:a:1}
-        if [[ $char == ${answer:a:1} ]]; then
+
+        if [ $char == ${answer:a:1} ]; then
             result[a]="\033[38;2;0;255;0m$char\033[0m"
             greens+=$char
             green_slots+=$a
@@ -66,7 +63,9 @@ for i in {1..6}; do
         if [[ $green_slots =~ $a ]]; then
             continue
         fi
+
         char=${attempt:a:1}
+
         for b in {0..4}; do
             if [[ $char == ${answer:b:1} && ! $slots$green_slots =~ $b ]]; then
                 result[a]="\033[38;2;255;255;0m$char\033[0m"
@@ -77,14 +76,17 @@ for i in {1..6}; do
                 break
             fi
         done
+
         if [[ $gray == true && ! $green_slots =~ $a ]]; then
             result[a]=$char
             grays+=$char
+
             if [[ $greens$yellows =~ $char ]]; then
                 if [[ ! $arg5 =~ $char ]]; then
-                    arg5+=$char$(echo $greens$yellows | grep -o "$char" | wc -l)
+                    arg5+=$char$(echo $greens$yellows | grep -o $char | wc -l)
                 fi
-                grays=$(echo $grays | tr -d "$char")
+
+                grays=$(echo $grays | tr -d $char)
             fi
         else
             gray=true
@@ -93,27 +95,29 @@ for i in {1..6}; do
 
     not_gray=$greens$yellows
 
-    while [[ $not_gray != "" ]]; do
-        count=$(echo $not_gray | grep -o "${not_gray:0:1}" | wc -l)
+    until [[ $not_gray == '' ]]; do
+        count=$(echo $not_gray | grep -o ${not_gray:0:1} | wc -l)
+
         if [[ $arg2 =~ ${not_gray:0:1} ]]; then
-            if [[ $count -gt $(echo $arg2 | cut -d "${not_gray:0:1}" -f2 | cut -c1) ]]; then
+            if [ $count > $(echo $arg2 | cut -d ${not_gray:0:1} -f2 | cut -c1) ]; then
                 arg2=${arg2/${not_gray:0:1}?/${not_gray:0:1}$count}
             fi
         else
             arg2+=${not_gray:0:1}$count
         fi
-        not_gray=$(echo $not_gray | tr -d "${not_gray:0:1}")
+
+        not_gray=$(echo $not_gray | tr -d ${not_gray:0:1})
     done
 
     output="\n********** $(echo ${result[*]} | tr -d [:space:]) ********** \033[38;2;66;255;255mTry #$i\033[0m"
 
-    if [[ $won == false ]]; then
+    if [ $won == false ]; then
         output+=" -> $($path/evaluator - $arg1 - $arg2 - $grays - $arg4 - $arg5)"
     fi
 
     echo -e "$output"
 
-    alphabet_print=""
+    alphabet_print=''
     all_greens+=$greens
     all_yellows+=$yellows
 
@@ -131,7 +135,7 @@ for i in {1..6}; do
 
     echo -e "$alphabet_print\n"
 
-    if [[ $won == true ]]; then
+    if [ $won == true ]; then
         echo -e "Congrats! Correct word was found in \033[38;2;66;255;255m$i\033[0m attempts."
         exec $SHELL
     fi
